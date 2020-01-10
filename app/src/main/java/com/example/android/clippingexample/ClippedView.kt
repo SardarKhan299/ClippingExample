038736a88f59f796ac3d89d94b6ea1492f72899b
Administrator <sardar@mypaperlessoffice.com>
2020-01-11 00:51:02 +0500

    Add dimens and strings for the project

---
+++ app/src/main/java/com/example/android/clippingexample/ClippedView.kt
@@ -0,0 +1,27 @@
+package com.example.android.clippingexample
+
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.util.Log
+import android.view.View
+
+class ClippedView@JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(context) {
+    init {
+        Log.d(this@ClippedView.javaClass.simpleName, "Init: ");
+    }
+
+    private val paint = Paint().apply {
+        // Smooth out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        strokeWidth = resources.getDimension(R.dimen.strokeWidth)
+        textSize = resources.getDimension(R.dimen.textSize)
+    }
+    private val path = Path()
+
+}
